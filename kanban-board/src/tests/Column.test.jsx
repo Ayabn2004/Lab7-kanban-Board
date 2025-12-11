357cdefaedf6a3242a675821246ad767a4f9608b
Ayabn2004 <sunfloweer21@gmail.com>
2025-12-11 16:20:15 +0100

    Column.test.jsx

--- kanban-board/src/tests/Column.test.jsx
+++ kanban-board/src/tests/Column.test.jsx
@@ -1,4 +1,4 @@
-import React from "react";              // ➜ obligatoire pour Jest
+import React from "react";             
 import { render, screen } from "@testing-library/react";
 import Column from "../components/Column";
 
